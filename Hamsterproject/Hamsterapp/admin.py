6a91fd8aaf76aa60101afb76fef57fd8a9c79c60
Nick <127136860+xomstor@users.noreply.github.com>
2023-12-27 19:27:58 +0300

    пффф
    пуш пуш

--- Hamsterproject/Hamsterapp/admin.py
+++ Hamsterproject/Hamsterapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FormUser
+
+class FormUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'age',]
+    list_display_links = ['name', 'email',]
+    list_filter = ['age',]
+
+admin.site.register(FormUser, FormUserAdmin)
--- Hamsterproject/Hamsterapp/models.py
+++ Hamsterproject/Hamsterapp/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 class FormUser(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,
+                            verbose_name="Ім'я",
+                            null = False,
+                            default = "Unknown",
+                            help_text="Введіть ваше Ім'я")
+    email = models.EmailField(verbose_name="Пошта",
+                            null = False,
+                            help_text="Введіть вашу пошту")
+    age = models.IntegerField(verbose_name="Вік",
+                            null = False,
+                            help_text="Введіть ваш вік")
+    def __str__(self):
+        return self.email
+    class Meta:
+        verbose_name = "Контактну інформацію користувачя"
+        verbose_name_plural = "Користувачі - контактні інформації"
+    
