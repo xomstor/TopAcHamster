417991529122130c0ebf3682c669485759b646af
Nick <127136860+xomstor@users.noreply.github.com>
2023-12-27 18:57:23 +0300

    +session + post in views

--- Hamsterproject/Hamsterapp/views.py
+++ Hamsterproject/Hamsterapp/views.py
@@ -6,12 +6,12 @@ class Hamsterhome(View):
     template_home = 'Hamsterapp/index.html'
     def get(self, request):
         ses = request.session.get('hist', False)
-        if ses:
-            request.session['hist'] = ses + 1
-            return HttpResponse("Вы вошли на сайт {} раз.".format(ses))
-        else:
-            request.session['hist'] = 1
-            return HttpResponse("Вы первыми вошли на сайт!")
+        # if ses:
+        #     request.session['hist'] = ses + 1
+        #     return HttpResponse("Вы вошли на сайт {} раз.".format(ses))
+        # else:
+        #     request.session['hist'] = 1
+        #     return HttpResponse("Вы первыми вошли на сайт!")
         return render(request, self.template_home)
     def post(self, request):
         print(request.POST)
