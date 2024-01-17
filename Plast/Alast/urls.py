b83f027c0b2ba956e10c87a1126b96ae43d97216
Nick <127136860+xomstor@users.noreply.github.com>
2024-01-17 20:30:36 +0300

    1701comm halfwaylesson

--- Plast/Alast/urls.py
+++ Plast/Alast/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views as v
 
 urlpatterns = [
-    re_path(r'home/?$', v.HomePage.as_view(), name = 'url_home' ),
+    path('', v.HomePage.as_view(), name = 'url_home' ),
     re_path(r'news/?$', v.NewsPage.as_view(), name = 'url_news' ),
     re_path(r'offer/?$', v.OfferPage.as_view(), name = 'url_offer' ),
 ]
