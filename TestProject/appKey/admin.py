611a1da2ffbebe2cdd1d0592059cb32dca757fa0
Nick <127136860+xomstor@users.noreply.github.com>
2024-01-29 20:47:11 +0300

    created two new models

--- TestProject/appKey/admin.py
+++ TestProject/appKey/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title',]
+    list_display_links = ['title',]
+
+    class Meta:
+        model = Category
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'desc', 'price', 'category', 'date']
+    list_display_links = ['title',]
+
+    class Meta:
+        model = Product
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
--- TestProject/appKey/models.py
+++ TestProject/appKey/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.TextField(
+    "Категория",
+    null=False,
+    help_text="Например: квартира"   
+    )
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
+class Product(models.Model):
+    title = models.TextField(
+        "Название",
+        null=False,
+        help_text="Например: Продаю земельный участок"
+    )
+    desc = models.TextField(
+        "Описание",
+        null=True,
+        blank=True
+    )
+    price = models.FloatField(
+        "Стоимость",
+        null=False
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT
+        )
+    date = models.DateTimeField(
+        auto_now_add=True
+        )
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
