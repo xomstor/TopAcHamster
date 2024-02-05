cb40dc5a181413c4c2070e62de172788d4ac25e0
Nick <127136860+xomstor@users.noreply.github.com>
2024-02-05 21:04:55 +0300

    help

--- TestProject/appKey/views.py
+++ TestProject/appKey/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views import View
+from django.db.models import Q
+from .models import Product, PhotoProduct, Category
 
 class HomeKey(View):
     def get(self, request):
-        return JsonResponse({
-            "status": 200
-            })
+        data = {
+            'products': Product.objects.all(),
+            'photos': PhotoProduct.objects.filter(photoforproduct__isnull=True)
+        }
+        return render(request, 'appKey/product/index.html', data)
+    def post(self, request):
+        if request.POST["method"] == "addCategory":
+           Category.objects.create(
+               title = request.POST["text"]
+           )
+        #    Category.objects.filter( # - СНАЧАЛА НАДО НАЙТИ ЧТО ОБНОВИТЬ И УДАЛИТЬ
+        #        title = request.POST["text"]
+        #    )
+        #    Category.objects.update(
+        #        title = request.POST["text"]
+        #    )
+        #    Category.objects.delete(
+        #        title = request.POST["text"]               
+        #    )
+        return redirect('url-home')
     
